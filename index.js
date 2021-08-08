9de9fcfe223064eff33324103f18f22e639ea6ce
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-08-08 13:32:07 -0700

    remove the e listener

--- index.js
+++ index.js
@@ -2,28 +2,35 @@
 
 const loadingBar = document.querySelector(".load__bar");
 const title = document.getElementById("title");
+const barContainer = document.querySelector(".container");
 
-//loadingBar.style.position = "fixed";
+barContainer.style.position = "fixed";
+loadingBar.style.position = "fixed";
+
 
 //test area
 console.log(title);
 console.log(loadingBar);
 
-let width = 1;
+//make the loading bar work
+let width = 0;
 
 let activeLoadBar = setInterval (function() {
     if (width >= 100) {
 clearInterval(activeLoadBar);
     } else {
         width++;
-        loadingBar.style.width = width;
+        loadingBar.style.width = width + "%";
     }
-}, 50)
+}, 10)
 
-console.log(activeLoadBar);
+//test area
+//console.log(activeLoadBar);
 
-title.addEventListener('click', function(e) {
-    e.preventDefault();
-    console.log("e listener worked");
-    return activeLoadBar;
-})
+//make it so the loading bar loads upong clicking
+
+// title.addEventListener('click', function(e) {
+//     e.preventDefault();
+//     console.log("e listener worked");
+//     return activeLoadBar;
+// })

6cd115a65cebb40f5d2e5fb7dd7832babfe2e805
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-08-09 16:06:27 -0700

    add a new function using onscroll

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 const loadingBar = document.querySelector(".load__bar");
 const title = document.getElementById("title");
 const barContainer = document.querySelector(".container");
+const body = document.getElementsByTagName("body");
 
+//affix / pin the load bar to top of screen
 barContainer.style.position = "fixed";
 loadingBar.style.position = "fixed";
 
@@ -15,14 +17,26 @@ console.log(loadingBar);
 //make the loading bar work
 let width = 0;
 
-let activeLoadBar = setInterval (function() {
-    if (width >= 100) {
-clearInterval(activeLoadBar);
-    } else {
-        width++;
-        loadingBar.style.width = width + "%";
-    }
-}, 10)
+// let activeLoadBar = setInterval (function() {
+//     if (width >= 100) {
+// clearInterval(activeLoadBar);
+//     } else {
+//         width++;
+//         loadingBar.style.width = width + "%";
+//     }
+// }, 10)
+
+//we can try another method
+
+body.onscroll = loadBar;
+
+console.log(loadBar);
+
+function loadBar(e) {
+    //e.target.scrollTop = width + "%";
+    console.log(e);
+}
+
 
 //test area
 //console.log(activeLoadBar);
